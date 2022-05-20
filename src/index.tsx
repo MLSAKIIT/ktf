486c0116252389eed26afe06dc3a1dff1138a274
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2022-05-20 07:41:55 +0530

    remove unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
 import App from './App'
-import NotFound from '@pages/_404';
 import '@styles/global.css'
 
 
